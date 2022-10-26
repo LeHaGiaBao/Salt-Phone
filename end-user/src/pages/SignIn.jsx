abcb191a13f70c4d1540578eae9c08772acea2b2
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-10-26 23:18:26 +0700

    Update project

--- end-user/src/pages/SignIn.jsx
+++ end-user/src/pages/SignIn.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SignInForm from '../assets/image/SignInForm.png'
 
 function SignIn() {
 	return (
 		<div>
 			<div>Viết ở đây</div>
+			<img src={SignInForm} alt='' /> {/Khai báo ảnh sử dụng như này/}
 		</div>
 	)
 }
