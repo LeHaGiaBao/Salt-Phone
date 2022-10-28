50eb838ec384fe59749b449ae0ebe454470dbc69
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-10-28 22:51:12 +0700

    Create Header container

---
+++ end-user/src/containers/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+import {FiSearch} from 'react-icons/fi'
+
+function Header() {
+	return (
+		<div>
+			<header>Viết code ở đây</header>
+		</div>
+	)
+}
+
+export default Header
