793eca2535d21e46cc8ff98ff47223bf3bea8953
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-10-29 16:07:46 +0700

    Add show password

--- end-user/src/App.jsx
+++ end-user/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-
 import './assets/css/base.css'
 import './assets/css/grid.css'
 import './assets/css/style.css'
 import './assets/css/responsive.css'
 import SignUpForm from './components/SignUpForm'
-
 
 function App() {
 	return (
