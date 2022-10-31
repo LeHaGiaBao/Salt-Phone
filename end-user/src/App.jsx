1b790e43a3c99ce3c8e292fc54d669f200d22183
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-10-31 20:27:54 +0700

    Update App.jsx

--- end-user/src/App.jsx
+++ end-user/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css'
-import SignInForm from './components/SignInForm/SignInForm'
-import NewPassword from './components/NewPassword/NewPassword'
-import NewPasswordNotice from './components/NewPasswordNotice/NewPasswordNotice'
-import Slider from './containers/Slider/Slider'
-import Header from './containers/Header/Header'
+import NewProduct from './components/NewProduct/NewProduct'
 
 function App() {
 	return (
 		<div>
-			<div>Abcd</div>
+			<div>
+				<NewProduct />
+			</div>
 		</div>
 	)
 }
