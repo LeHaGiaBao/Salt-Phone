55692cf7844c3f0e7f1f44d52adbca0ea3d29293
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-10-31 16:08:42 +0700

    Add image slider container

--- end-user/src/App.jsx
+++ end-user/src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import SignInForm from './components/SignInForm/SignInForm'
 import NewPassword from './components/NewPassword/NewPassword'
 import NewPasswordNotice from './components/NewPasswordNotice/NewPasswordNotice'
+import Slider from './containers/Slider/Slider'
+import Header from './containers/Header/Header'
 
 function App() {
 	return (
 		<div>
-			<div>
-				<NewPassword />
-			</div>
+			<div>Abcd</div>
 		</div>
 	)
 }
