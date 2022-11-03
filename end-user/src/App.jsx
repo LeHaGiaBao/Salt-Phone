23e7ea1115c8f9da5a9badf8214e282df12221b1
Thanh Mai <88275640+thenmeyy@users.noreply.github.com>
2022-11-03 09:07:01 +0700

    Add Best Seller Products

--- end-user/src/App.jsx
+++ end-user/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './App.css'
 import SignInForm from './components/SignInForm/SignInForm'
 import NewPassword from './components/NewPassword/NewPassword'
-import NewPasswordNotice from './components/NewPasswordNotice/NewPasswordNotice'
+import BestSellerProducts from './components/BestSellerProducts/BestSellerProducts'
 
 function App() {
 	return (
 		<div>
 			<div>
-				<NewPassword />
+				<BestSellerProducts />
 			</div>
 		</div>
 	)
