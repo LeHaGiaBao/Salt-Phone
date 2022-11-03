20c4022cf01b273c595a3c97ded0ba4d96c520a5
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-11-03 22:36:55 +0700

    Update App.jsx

--- end-user/src/App.jsx
+++ end-user/src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import './App.css'
-import SignInForm from './components/SignInForm/SignInForm'
-import NewPassword from './components/NewPassword/NewPassword'
-import NewPasswordNotice from './components/NewPasswordNotice/NewPasswordNotice'
+import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import RecoverPassword from './pages/RecoverPassword'
+import RecoverPasswordOTP from './pages/RecoverPasswordOTP'
+import NewPassword from './pages/NewPassword'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/SignIn',
+		element: <SignIn />,
+	},
+	{
+		path: '/SignUp',
+		element: <SignUp />,
+	},
+	{
+		path: '/RecoverPassword',
+		element: <RecoverPassword />,
+	},
+	{
+		path: '/RecoverPasswordOTP',
+		element: <RecoverPasswordOTP />,
+	},
+	{
+		path: '/NewPassword',
+		element: <NewPassword />,
+	},
+])
 
 function App() {
 	return <RouterProvider router={router} />
