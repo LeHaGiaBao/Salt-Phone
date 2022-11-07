8516c4ad4c83f7b3bcb4d51d684725042b39441d
21Jan <nguyenutt2101@gmail.com>
2022-11-07 21:17:41 +0700

    Add Tag & ListProduct UI & ProductCart UI

--- end-user/src/components/ListProducts/ListProducts.jsx
+++ end-user/src/components/ListProducts/ListProducts.jsx
@@ -3,8 +3,187 @@ import './ListProducts.css'
 
 function ListProducts() {
 	return (
-		<div>
-			<div>Code nội dung vào đây</div>
+		<div className='ListProducts'>
+			<div className='nav'>
+				<ul>
+					<li>
+						<a href='#'>Nổi bật</a>
+					</li>
+					<li>
+						<a href='#'>Tai nghe</a>
+					</li>
+					<li>
+						<a href='#'>Pin dự phòng</a>
+					</li>
+					<li>
+						<a href='#'>Cáp & Sạc</a>
+					</li>
+					<li>
+						<a href='#'>Ốp lưng</a>
+					</li>
+				</ul>
+			</div>
+			<div className='products'>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'>Mới</p>
+					<h2 className='product-name'>AirPods Pro 2nd MagSafe Charge</h2>
+					<p className='product-price'>6.990.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'>Mới</p>
+					<h2 className='product-name'>AirPods Pro MagSafe Charge</h2>
+					<p className='product-price'>5.190.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'></p>
+					<h2 className='product-name'>AirPods 2 Bluetooth</h2>
+					<p className='product-price'>2.990.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'></p>
+					<h2 className='product-name'>AirPods 3 Wireless Charge</h2>
+					<p className='product-price'>4.490.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'>Mới</p>
+					<h2 className='product-name'>AirPods Pro 2nd MagSafe Charge</h2>
+					<p className='product-price'>6.990.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'>Mới</p>
+					<h2 className='product-name'>AirPods Pro MagSafe Charge</h2>
+					<p className='product-price'>5.190.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'></p>
+					<h2 className='product-name'>AirPods 2 Bluetooth</h2>
+					<p className='product-price'>2.990.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'></p>
+					<h2 className='product-name'>AirPods 3 Wireless Charge</h2>
+					<p className='product-price'>4.490.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>{' '}
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'>Mới</p>
+					<h2 className='product-name'>AirPods Pro 2nd MagSafe Charge</h2>
+					<p className='product-price'>6.990.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'>Mới</p>
+					<h2 className='product-name'>AirPods Pro MagSafe Charge</h2>
+					<p className='product-price'>5.190.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'></p>
+					<h2 className='product-name'>AirPods 2 Bluetooth</h2>
+					<p className='product-price'>2.990.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+				<div className='product'>
+					<img
+						src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000'
+						alt=''
+					/>
+					<p className='product-newtag'></p>
+					<h2 className='product-name'>AirPods 3 Wireless Charge</h2>
+					<p className='product-price'>4.490.000đ</p>
+					<button className='product-buynow'>Mua ngay</button>
+					<a className='product-more' href='#'>
+						Tìm hiểu thêm &gt;
+					</a>
+				</div>
+			</div>
+			<a className='more' href='#'>
+				Xem thêm sản phẩm &gt;
+			</a>
 		</div>
 	)
 }
