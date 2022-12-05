87ad62a5cd5e7ad2cb0d9cd9b9551639c5388ca1
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-12-05 23:55:59 +0700

    Update project

--- web-admin/src/components/Tables/Cart/CartTable.jsx
+++ web-admin/src/components/Tables/Cart/CartTable.jsx
@@ -1,7 +1,8 @@
-import {Table} from 'antd'
+import React, {useState, useEffect, useReducer, useRef} from 'react'
+import {Table, Input, Space, Button} from 'antd'
 import axios from 'axios'
-import React, {useState} from 'react'
-import {useEffect} from 'react'
+import Highlighter from 'react-highlight-words'
+import {SearchOutlined} from '@ant-design/icons'
 
 function CartTable(props) {
 	const [state, setState] = useState([])
@@ -9,6 +10,108 @@ function CartTable(props) {
 	useEffect(() => {
 		getData()
 	}, [])
+
+	const [searchText, setSearchText] = useState('')
+	const [searchedColumn, setSearchedColumn] = useState('')
+
+	const searchInput = useRef(null)
+
+	const handleSearch = (selectedKeys, confirm, dataIndex) => {
+		confirm()
+		setSearchText(selectedKeys[0])
+		setSearchedColumn(dataIndex)
+	}
+
+	const handleReset = (clearFilters) => {
+		clearFilters()
+		setSearchText('')
+	}
+
+	const getColumnSearchProps = (dataIndex) => ({
+		filterDropdown: ({
+			setSelectedKeys,
+			selectedKeys,
+			confirm,
+			clearFilters,
+			close,
+		}) => (
+			<div
+				style={{
+					padding: 8,
+				}}
+				onKeyDown={(e) => e.stopPropagation()}>
+				<Input
+					ref={searchInput}
+					placeholder={`Search ${dataIndex}`}
+					value={selectedKeys[0]}
+					onChange={(e) =>
+						setSelectedKeys(e.target.value ? [e.target.value] : [])
+					}
+					onPressEnter={() =>
+						handleSearch(selectedKeys, confirm, dataIndex)
+					}
+					style={{
+						marginBottom: 8,
+						display: 'block',
+					}}
+				/>
+				<Space>
+					<Button
+						type='primary'
+						onClick={() =>
+							handleSearch(selectedKeys, confirm, dataIndex)
+						}
+						icon={<SearchOutlined />}
+						size='small'
+						style={{
+							width: 90,
+							backgroundColor: '#EE8488',
+						}}>
+						Search
+					</Button>
+					<Button
+						type='link'
+						size='small'
+						onClick={() => {
+							close()
+						}}>
+						close
+					</Button>
+				</Space>
+			</div>
+		),
+
+		filterIcon: (filtered) => (
+			<SearchOutlined
+				style={{
+					color: filtered ? '#1890ff' : undefined,
+				}}
+			/>
+		),
+		onFilter: (value, record) =>
+			record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+		onFilterDropdownOpenChange: (visible) => {
+			if (visible) {
+				setTimeout(() => searchInput.current?.select(), 100)
+			}
+		},
+		render: (text) =>
+			searchedColumn === dataIndex ? (
+				<Highlighter
+					highlightStyle={{
+						backgroundColor: '#EE8488',
+						color: 'white',
+						fontWeight: 'bolder',
+						padding: 0,
+					}}
+					searchWords={[searchText]}
+					autoEscape
+					textToHighlight={text ? text.toString() : ''}
+				/>
+			) : (
+				text
+			),
+	})
 
 	const getData = async () => {
 		await axios.get('http://127.0.0.1:8000/api/khachhang').then((res) => {})
@@ -19,6 +122,7 @@ function CartTable(props) {
 			title: 'Mã đơn hàng',
 			dataIndex: 'cartid',
 			width: '5%',
+			...getColumnSearchProps('cartid'),
 		},
 		{
 			title: 'Ngày mua hàng',
@@ -29,6 +133,7 @@ function CartTable(props) {
 			title: 'Địa chỉ giao hàng',
 			dataIndex: 'address',
 			width: '10%',
+			...getColumnSearchProps('address'),
 		},
 		{
 			title: 'Trạng thái đơn hàng',
@@ -39,6 +144,7 @@ function CartTable(props) {
 			title: 'Khách hàng',
 			dataIndex: 'customer',
 			width: '9%',
+			...getColumnSearchProps('customer'),
 		},
 		{
 			title: 'Thao tác',
