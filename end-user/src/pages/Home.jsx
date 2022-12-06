390c68be374135d6db6547feb62e6e188f970c99
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-12-06 10:37:11 +0700

    Update project

--- end-user/src/pages/Home.jsx
+++ end-user/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import Header from '../containers/Header/Header'
 import Slider from '../containers/Slider/Slider'
 import NewProduct from '../components/NewProduct/NewProduct'
@@ -7,17 +7,13 @@ import TopAccessory from '../components/TopAccessory/TopAccessory'
 import Footer from '../containers/Footer/Footer'
 
 function Home() {
-	useEffect(() => {
-		document.title = 'Salt Phone'
-	}, 1)
-
 	return (
 		<div>
 			<Header />
 			<Slider />
 			<NewProduct />
-			{/* <BestSellerProducts />
-			<TopAccessory /> */}
+			<BestSellerProducts />
+			<TopAccessory />
 			<Footer />
 		</div>
 	)
--- end-user/src/pages/PageNotFound/PageNotFound.jsx
+++ end-user/src/pages/PageNotFound/PageNotFound.jsx
@@ -2,12 +2,9 @@ import './PageNotFound.css'
 import {BsArrowLeftShort} from 'react-icons/bs'
 import NotFoundNum4 from '../../assets/image/NotFoundNum4.png'
 import NotFoundNum0 from '../../assets/image/NotFoundNum0.png'
-import {Link, useRouteError} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 function PageNotFound() {
-	const error = useRouteError()
-	console.log(error)
-
 	return (
 		<div className='PageNotFound'>
 			<div className='PageNotFound-img'>
