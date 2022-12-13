29e38b41ac81a354cf00f34652b081da91de0f3e
LeHaGiaBao <20520141@gm.uit.edu.vn>
2022-12-13 14:05:13 +0700

    Fix bug login end user

--- end-user/src/components/ProductInformation/ProductInformation.jsx
+++ end-user/src/components/ProductInformation/ProductInformation.jsx
@@ -65,14 +65,6 @@ function ProductInformation() {
 									{phone.giadienthoai} đ
 								</div>
 								<table className='content_product-order_select'>
-									{/* <tr className='content_product-order_select_item'>
-										<td className='content_product-order_select_label'>
-											Vận chuyển
-										</td>
-										<td className='content_product-order_select_ship'>
-											Chọn địa chỉ nhận hàng
-										</td>
-									</tr> */}
 									<tr className='content_product-order_select_item'>
 										<td className='content_product-order_select_label'>
 											Số lượng
